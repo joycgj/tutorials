4922225f1dd38d09552eabf227a56aa59f986eb0
joycgj <chengaojie001@gmail.com>
2025-02-18 10:58:15 +0800

    add exception

--- libraries-data-db/src/main/java/com/baeldung/libraries/debezium/service/CustomerService.java
+++ libraries-data-db/src/main/java/com/baeldung/libraries/debezium/service/CustomerService.java
@@ -4,10 +4,12 @@ import com.baeldung.libraries.debezium.entity.Customer;
 import com.baeldung.libraries.debezium.repository.CustomerRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.debezium.data.Envelope.Operation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+@Slf4j
 @Service
 public class CustomerService {
 
@@ -18,13 +20,18 @@ public class CustomerService {
     }
 
     public void replicateData(Map<String, Object> customerData, Operation operation) {
-        final ObjectMapper mapper = new ObjectMapper();
-        final Customer customer = mapper.convertValue(customerData, Customer.class);
 
-        if (Operation.DELETE == operation) {
-            customerRepository.deleteById(customer.getId());
-        } else {
-            customerRepository.save(customer);
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final Customer customer = mapper.convertValue(customerData, Customer.class);
+
+            if (Operation.DELETE == operation) {
+                customerRepository.deleteById(customer.getId());
+            } else {
+                customerRepository.save(customer);
+            }
+        } catch (IllegalArgumentException e) {
+            log.error("error while replicateData", e);
         }
     }
 }
